577a28c167ea7f293ebbe3e8b55e3801ed08ad2b
lexto92@gmail.com <lexto92@gmail.com>
2020-02-01 12:35:07 +0200

    Создал и подключил модуль для загрузки\выгрузки данных с\на сервер

---
+++ js/backend.js
@@ -0,0 +1,60 @@
+'use strict';
+(function () {
+  var METHOD = {
+    POST: 'POST',
+    GET: 'GET'
+  };
+
+  var URL = {
+    LOAD: 'https://js.dump.academy/kekstagram/data',
+    UPLOAD: 'https://js.dump.academy/kekstagram'
+  };
+
+  var STATUS = {
+    GOOD: 200
+  };
+  var TIMEOUT = 5000;
+
+  window.sendRequest = function (onSuccess, createEror) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      if (xhr.status === STATUS.GOOD) {
+        onSuccess(xhr.response);
+      } else {
+        createEror('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      createEror('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      createEror('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+    xhr.timeout = TIMEOUT;
+    return xhr;
+  };
+
+  var load = function (onSuccess, createEror) {
+    var xhr = window.sendRequest(onSuccess, createEror);
+    xhr.open(METHOD.GET, URL.LOAD);
+    xhr.send();
+  };
+
+  var upload = function (data, onSuccess, createEror) {
+    var xhr = window.sendRequest(onSuccess, createEror);
+    xhr.open(METHOD.POST, URL.UPLOAD);
+    xhr.send(data);
+
+  };
+  document.addEventListener('click', function () {
+    load(function (data) {
+      console.log(data);
+    });
+  });
+
+  window.backend = {
+    load: load,
+    upload: upload
+  };
+})();
